0f64485cd7cd7149150b68444bf4286d7f111e9f
kimseungyeung <seungyeung1@naver.com>
2019-08-20 19:51:57 +0900

    .

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -44,87 +44,7 @@ document.addEventListener("paste", function(e) {
     }
 });
 
-// var options = {
-//     modes: ['code', 'tree'],
-//     mode: 'tree',
-//     languages: {
-//         ko: {
-//             'array': '배열',
-//             'auto': '자동',
-//             'appendText': '추가',
-//             'appendTitle': '이 항목 다음에 새 항목을 추가합니다. (Ctrl+Shift+Ins)',
-//             'appendSubmenuTitle': '추가할 항목의 데이타 타입을 선택합니다.',
-//             'appendTitleAuto': '새 항목을 추가합니다. (Ctrl+Shift+Ins)',
-//             'ascending': '정방향',
-//             'ascendingTitle': 'Sort the childs of this ${type} in ascending order',
-//             'actionsMenu': '메뉴를 열려면 클릭하세요. (Ctrl+M)',
-//             'collapseAll': 'Collapse all fields',
-//             'descending': '역방향',
-//             'descendingTitle': 'Sort the childs of this ${type} in descending order',
-//             'drag': 'Drag to move this field (Alt+Shift+Arrows)',
-//             'duplicateKey': '중복키',
-//             'duplicateText': '복제',
-//             'duplicateTitle': '선택된 항목을 복제합니다. (Ctrl+D)',
-//             'duplicateField': '이 항목을 복제합니다. (Ctrl+D)',
-//             'empty': '공백',
-//             'expandAll': 'Expand all fields',
-//             'expandTitle': '이 항목을 펼치거나 닫으려면 클릭하세요. (Ctrl+E). \nCtrl+Click하면 하위 자식 노드까지 포함해서 모두 펼치거나 닫습니다.',
-//             'insert': '삽입',
-//             'insertTitle': 'Insert a new field with type \'auto\' before this field (Ctrl+Ins)',
-//             'insertSub': '삽입할 항목의 데이타 타입을 선택합니다.',
-//             'object': '객체',
-//             'ok': '확인',
-//             'redo': '재실행 (Ctrl+Shift+Z)',
-//             'removeText': '제거',
-//             'removeTitle': '선택된 항목을 제거합니다. (Ctrl+Del)',
-//             'removeField': '이 항목을 제거합니다. (Ctrl+Del)',
-//             'selectNode': '노드를 선택하세요...',
-//             'showAll': 'show all',
-//             'showMore': 'show more',
-//             'showMoreStatus': 'displaying ${visibleChilds} of ${totalChilds} items.',
-//             'sort': '정렬',
-//             'sortTitle': 'Sort the childs of this ${type}',
-//             'sortTitleShort': 'Sort contents',
-//             'sortFieldLabel': 'Field:',
-//             'sortDirectionLabel': 'Direction:',
-//             'sortFieldTitle': 'Select the nested field by which to sort the array or object',
-//             'sortAscending': '정방향',
-//             'sortAscendingTitle': 'Sort the selected field in ascending order',
-//             'sortDescending': '역방향',
-//             'sortDescendingTitle': 'Sort the selected field in descending order',
-//             'string': '문자열',
-//             'transform': 'Transform',
-//             'transformTitle': 'Filter, sort, or transform the childs of this ${type}',
-//             'transformTitleShort': 'Filter, sort, or transform contents',
-//             'transformQueryTitle': 'Enter a JMESPath query',
-//             'transformWizardLabel': 'Wizard',
-//             'transformWizardFilter': 'Filter',
-//             'transformWizardSortBy': 'Sort by',
-//             'transformWizardSelectFields': 'Select fields',
-//             'transformQueryLabel': 'Query',
-//             'transformPreviewLabel': 'Preview',
-//             'type': '타입',
-//             'typeTitle': '이 필드의 타입을 변경합니다.',
-//             'openUrl': 'Ctrl+Click or Ctrl+Enter to open url in new window',
-//             'undo': '실행취소 (Ctrl+Z)',
-//             'validationCannotMove': 'Cannot move a field into a child of itself',
-//             'autoType': '입력한 값에 따라 데이타 타입을 자동으로 결정합니다.',
-//             'objectType': 'Field type "object". An object contains an unordered set of key/value pairs.',
-//             'arrayType': 'Field type "array". An array contains an ordered collection of values.',
-//             'stringType': 'Field type "string". Field type is not determined from the value, but always returned as string.'
-//         }
-//     },
-//
-// };
-//
-//
-// $(function() {
-//     var contatiner=document.getElementById('editor_holder');
-//     // console.log('xxx', contatiner.is('*'));
-//      editor = new JSONEditor(contatiner,options);
-//
-//     // editor.enable();
-// });
+
 function test() {
     var ed1 =parseInt($('#edt_1').val());
     var ed2 =$('#edt_2').val();
@@ -160,7 +80,7 @@ function test() {
             "country": ed2,
             "otherMoney": ed3,
             "my_Money": ed4,
-            "accdDate":  ed5,
+            "accddate":  ed5,
             "hspStDt": ed6,
             "count":  ed7,
             "hspEndDt":  ed8,
@@ -214,11 +134,19 @@ function test() {
 
 
     }).done(function (result) {
+        var text="";
+        var index=Object.keys(result).length;
 
+        $.each(result,function (key2,value2) {
+            index--;
+            var ff = result[index];
+            text += key2+":"+JSON.stringify(ff) +"\n";
 
+        });
        // alert(html);
-        $("#result").text(JSON.stringify(result));
-
+        $("#result").text(text);
+        $("#result").css("color","white");
+        $("#result").css("font-size",20);
 
     }).fail(function () {
         alert("실행 실패");
