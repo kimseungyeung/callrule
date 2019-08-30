0783d3502b7d98753414b1c8ade4e0bed6fcfcf2
kimseungyeung <seungyeung1@naver.com>
2019-08-30 10:31:16 +0900

    .

--- src/main/java/com/rule/test/controller/SubPageController.java
+++ src/main/java/com/rule/test/controller/SubPageController.java
@@ -3,6 +3,7 @@ package com.rule.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SubPageController {
@@ -14,8 +15,8 @@ public class SubPageController {
     }
 
     @RequestMapping(path ="/sub2")
-    public String sub2(Model m){
-        m.addAttribute("text","서브메인2화면입니다.");
+    public String sub2(Model m,@RequestParam String k){
+        m.addAttribute("text",k);
         return "sub2";
     }
 }
